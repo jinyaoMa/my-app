fc6421b4b90af6503e10196d978252a72f46301b
马锦尧 <jinyao.ma@outlook.com>
2023-05-10 14:06:21 +0800

    .

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,13 +1,42 @@
 package logger
 
-import "log"
+import (
+	"io"
+	"log"
+)
 
 type ILogger interface {
+	Fatal(v ...any)
+	Fatalf(format string, v ...any)
+	Fatalln(v ...any)
+	Flags() int
+	Output(calldepth int, s string) error
+	Panic(v ...any)
+	Panicf(format string, v ...any)
+	Panicln(v ...any)
+	Prefix() string
+	Print(v ...any)
+	Printf(format string, v ...any)
+	Println(v ...any)
+	SetFlags(flag int)
+	SetOutput(w io.Writer)
+	SetPrefix(prefix string)
+	Writer() io.Writer
 }
 
 type Logger struct {
 	*log.Logger
 	*Options
+}
+
+// Flags implements ILogger
+func (l *Logger) Flags() int {
+	return l.Options.Flags
+}
+
+// Writer implements ILogger
+func (l *Logger) Writer() io.Writer {
+	return l.Options.Writer
 }
 
 func NewLogger(opts *Options) ILogger {
