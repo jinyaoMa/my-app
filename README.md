8efed15a4a98c698351ac70f5d45d404dfa28c03
jinyaoMa <jinyao.ma@outlook.com>
2023-05-16 22:24:54 +0800

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+### todo
+
+- [ ] entity.Option.Value encryption
+- [ ] jwt
--- backend/pkg/database/entity/option.go
+++ backend/pkg/database/entity/option.go
@@ -4,8 +4,9 @@ import iSnowflake "my-app/backend/pkg/snowflake/interfaces"
 
 type Option struct {
 	Entity
-	Key   string `xorm:"size:256; unique; index"`
-	Value string `xorm:"size:256"`
+	Key       string `gorm:"size:100; unique; index"`
+	Value     string `gorm:"size:255"`
+	Encrypted bool   `gorm:""`
 }
 
 func NewOption(snowflake iSnowflake.ISnowflake, option *Option) *Option {
