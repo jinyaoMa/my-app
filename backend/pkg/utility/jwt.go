4e774c03df78efdb22eb68bb8920ff1c7afe06cb
马锦尧 <jinyao.ma@outlook.com>
2023-05-18 09:11:47 +0800

    .

--- backend/pkg/utility/jwt.go
+++ backend/pkg/utility/jwt.go
@@ -1,6 +1,8 @@
 package utility
 
 import (
+	"errors"
+	"fmt"
 	"my-app/backend/pkg/utility/interfaces"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -19,8 +21,27 @@ func (j *Jwt[TClaims]) GetToken() (token string, err error) {
 }
 
 // ParseToken implements interfaces.IJwt
-func (*Jwt[TClaims]) ParseToken(token string) (claims TClaims, err error) {
-	panic("unimplemented")
+func (j *Jwt[TClaims]) ParseToken(token string) (claims TClaims, err error) {
+	ready, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(j.key), nil
+	})
+
+	var ok bool
+	if claims, ok = ready.Claims.(TClaims); ok && ready.Valid {
+		return
+	} else if errors.Is(err, jwt.ErrTokenMalformed) {
+		err = errors.New("That's not even a token")
+	} else if errors.Is(err, jwt.ErrTokenSignatureInvalid) {
+		// Invalid signature
+		err = errors.New("Invalid signature")
+	} else if errors.Is(err, jwt.ErrTokenExpired) || errors.Is(err, jwt.ErrTokenNotValidYet) {
+		// Token is either expired or not active yet
+		err = errors.New("Token is either expired or not active yet")
+	} else {
+		err = errors.New(fmt.Sprint("Couldn't handle this token:", err))
+	}
+
+	return
 }
 
 func NewJwt[TClaims jwt.Claims](claims TClaims, key string) interfaces.IJwt[TClaims] {
