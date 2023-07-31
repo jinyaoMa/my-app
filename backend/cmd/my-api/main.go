d902931a4a7f0d8276235d2f9a6df9d9c10ed14d
Jinyao Ma <jinyao.ma@outlook.com>
2023-07-31 23:12:50 +0800

    .

---
+++ backend/cmd/my-api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"my-app/backend/pkg/server"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	s := server.New()
+	if s.Start(&server.Option{}) {
+		println("start")
+	}
+
+	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+	<-c // This blocks the main thread until an interrupt is received
+
+	if s.Stop(func() {
+		println("try to stop...")
+	}) {
+		println("exit")
+	}
+}
