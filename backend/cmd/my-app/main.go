41d343c5c872d267aa3d8e76d5a5746995f123ac
Jinyao Ma <jinyao.ma@outlook.com>
2023-09-13 18:23:55 +0800

    .

--- backend/cmd/my-app/main.go
+++ backend/cmd/my-app/main.go
@@ -7,7 +7,11 @@ import (
 
 	"github.com/devfeel/mapper"
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
@@ -18,20 +22,98 @@ func main() {
 	optionService := service.NewOption(app.DB(), mapper.NewMapper())
 
 	// Create application with options
+
 	err := wails.Run(&options.App{
-		Title:             app.T().AppName,
-		Width:             1024,
-		Height:            720,
-		Assets:            frontend.Assets,
+		Title:         app.T().AppName,
+		Width:         1024,
+		Height:        720,
+		DisableResize: false,
+		// Fullscreen:         false,
+		WindowStartState:  options.Normal,
+		Frameless:         true,
+		MinWidth:          1024,
+		MinHeight:         600,
+		MaxWidth:          1200,
+		MaxHeight:         720,
+		StartHidden:       false,
 		HideWindowOnClose: true,
-		BackgroundColour:  &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:         wailsapp.startup,
+		BackgroundColour:  &options.RGBA{R: 0, G: 0, B: 0, A: 255},
+		AlwaysOnTop:       false,
+		AssetServer: &assetserver.Options{
+			Assets:     frontend.Assets,
+			Handler:    nil,
+			Middleware: nil,
+		},
+		// Menu:               app.applicationMenu(),
+		Logger:             nil,
+		LogLevel:           logger.DEBUG,
+		LogLevelProduction: logger.ERROR,
+		OnStartup:          wailsapp.startup,
+		// OnDomReady:         app.domready,
+		// OnShutdown:         app.shutdown,
+		// OnBeforeClose:      app.beforeClose,
+		CSSDragProperty:                  "--wails-draggable",
+		CSSDragValue:                     "drag",
+		EnableDefaultContextMenu:         false,
+		EnableFraudulentWebsiteDetection: false,
+		// ZoomFactor:           1.0,
+		// IsZoomControlEnabled: false,
 		Bind: []interface{}{
 			wailsapp,
 			optionService,
 		},
+		ErrorFormatter: func(err error) any { return err.Error() },
 		Windows: &windows.Options{
+			WebviewIsTransparent:              false,
+			WindowIsTranslucent:               false,
+			BackdropType:                      windows.Mica,
+			DisableWindowIcon:                 false,
+			DisableFramelessWindowDecorations: false,
+			// WebviewUserDataPath:               "",
+			// WebviewBrowserPath:                "",
 			Theme: app.THEME(),
+			// CustomTheme: &windows.ThemeSettings{
+			//     DarkModeTitleBar:   windows.RGB(20, 20, 20),
+			//     DarkModeTitleText:  windows.RGB(200, 200, 200),
+			//     DarkModeBorder:     windows.RGB(20, 0, 20),
+			//     LightModeTitleBar:  windows.RGB(200, 200, 200),
+			//     LightModeTitleText: windows.RGB(20, 20, 20),
+			//     LightModeBorder:    windows.RGB(200, 200, 200),
+			// },
+			// // User messages that can be customised
+			// Messages *windows.Messages
+			// // OnSuspend is called when Windows enters low power mode
+			// OnSuspend func()
+			// // OnResume is called when Windows resumes from low power mode
+			// OnResume func(),
+			// WebviewGpuDisabled: false,
+		},
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: true,
+				HideTitle:                  false,
+				HideTitleBar:               false,
+				FullSizeContent:            false,
+				UseToolbar:                 false,
+				HideToolbarSeparator:       true,
+			},
+			Appearance:           mac.NSAppearanceNameDarkAqua,
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  false,
+			About: &mac.AboutInfo{
+				Title:   "My Application",
+				Message: "© 2021 Me",
+				// Icon:    icon,
+			},
+		},
+		Linux: &linux.Options{
+			// Icon: icon,
+			WindowIsTranslucent: false,
+			WebviewGpuPolicy:    linux.WebviewGpuPolicyAlways,
+			ProgramName:         "wails",
+		},
+		Debug: options.Debug{
+			OpenInspectorOnStartup: false,
 		},
 	})
 
--- go.mod
+++ go.mod
@@ -3,19 +3,19 @@ module my-app
 go 1.20
 
 require (
-	github.com/gofiber/fiber/v2 v2.48.0
+	github.com/gofiber/fiber/v2 v2.49.1
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-	gorm.io/driver/sqlite v1.5.2
-	gorm.io/gorm v1.25.2
+	gorm.io/driver/sqlite v1.5.3
+	gorm.io/gorm v1.25.4
 )
 
 require (
 	github.com/devfeel/mapper v0.7.13
 	github.com/getlantern/systray v1.2.2
-	github.com/shirou/gopsutil/v3 v3.23.6
+	github.com/shirou/gopsutil/v3 v3.23.8
 	gopkg.in/ini.v1 v1.67.0
-	gorm.io/plugin/dbresolver v1.4.2
+	gorm.io/plugin/dbresolver v1.4.7
 )
 
 require (
@@ -29,16 +29,16 @@ require (
 	github.com/getlantern/ops v0.0.0-20230519221840-1283e026181c // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/labstack/echo/v4 v4.10.2 // indirect
+	github.com/labstack/echo/v4 v4.11.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
-	github.com/leaanthony/go-ansi-parser v1.6.0 // indirect
+	github.com/leaanthony/go-ansi-parser v1.6.1 // indirect
 	github.com/leaanthony/gosod v1.0.3 // indirect
 	github.com/leaanthony/slicer v1.6.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -51,27 +51,28 @@ require (
 	github.com/samber/lo v1.38.1 // indirect
 	github.com/tkrajina/go-reflector v0.5.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.48.0 // indirect
+	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/wailsapp/go-webview2 v1.0.5 // indirect
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
-	go.opentelemetry.io/otel v1.16.0 // indirect
-	go.opentelemetry.io/otel/metric v1.16.0 // indirect
-	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	go.opentelemetry.io/otel v1.18.0 // indirect
+	go.opentelemetry.io/otel/metric v1.18.0 // indirect
+	go.opentelemetry.io/otel/trace v1.18.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	go.uber.org/zap v1.25.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 )
 
 require (
 	dario.cat/mergo v1.0.0
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/wailsapp/wails/v2 v2.5.1
-	golang.org/x/crypto v0.11.0
-	golang.org/x/net v0.12.0 // indirect
+	github.com/wailsapp/wails/v2 v2.6.0
+	golang.org/x/crypto v0.13.0
+	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sync v0.3.0
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
