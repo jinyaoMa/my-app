56c80caaf386a8920a68f502426f8123607a3169
Jinyao Ma <jinyao.ma@outlook.com>
2023-09-14 12:57:49 +0800

    .

--- backend/cmd/my-tray/menuitems/root.go
+++ backend/cmd/my-tray/menuitems/root.go
@@ -2,8 +2,11 @@ package menuitems
 
 import (
 	"context"
+	"fmt"
 	"my-app/backend/internal/app"
 	"my-app/backend/pkg/tray"
+
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 type root struct {
@@ -28,7 +31,30 @@ func (*root) Title() string {
 
 // Tooltip implements tray.Interface.
 func (*root) Tooltip() string {
-	return app.T().AppName
+	T := app.T()
+
+	webServiceState := T.APIService.Disabled
+	if app.API().IsRunning() {
+		webServiceState = T.APIService.Enabled
+	}
+
+	displayLanguageText := T.Lang.Text
+
+	colorThemeText := T.ColorTheme.System
+	switch app.THEME() {
+	case windows.Light:
+		colorThemeText = T.ColorTheme.Light
+	case windows.Dark:
+		colorThemeText = T.ColorTheme.Dark
+	}
+
+	return fmt.Sprintf(
+		"%s\n%s\n%s\n%s",
+		T.AppName,
+		T.APIService.Label+webServiceState,
+		T.DisplayLanguage.Label+displayLanguageText,
+		T.ColorTheme.Label+colorThemeText,
+	)
 }
 
 func newRoot(ctx context.Context) tray.IMenuItemBase {
