2cdf7ba427a3a0e0e0600cce91264e37faa5b9c3
Jinyao Ma <jinyao.ma@outlook.com>
2023-09-18 13:46:56 +0800

    .

--- frontend/components/icon/index.ts
+++ frontend/components/icon/index.ts
@@ -4,5 +4,3 @@ import Icon from "./src/icon.vue";
 
 export const MyIcon = withInstall(Icon);
 export default MyIcon;
-
-export * from "./src/icon";
--- frontend/components/icon/src/icon.vue
+++ frontend/components/icon/src/icon.vue
@@ -1,2 +1,39 @@
-<script setup></script>
-<template></template>
+<template>
+  <i
+    :class="{
+      [`my-icon-${props.name}`]: true,
+      fixWidth: props.fixWidth,
+    }"
+  ></i>
+</template>
+
+<script setup lang="ts">
+import "@jinyaoma/my-app-icons/dist/my-icon.scss";
+import { withDefaults } from "vue";
+
+defineOptions({
+  name: 'MyIcon',
+  inheritAttrs: false,
+})
+
+const props = withDefaults(
+  defineProps<{
+    name?: string;
+    fixWidth?: boolean;
+  }>(),
+  {
+    name: "jinyao-ma",
+    fixWidth: false,
+  }
+);
+</script>
+
+<style lang="scss">
+[class*="my-icon-"] {
+  display: inline-block;
+  &.fixWidth {
+    width: 1.3em;
+    text-align: center;
+  }
+}
+</style>
