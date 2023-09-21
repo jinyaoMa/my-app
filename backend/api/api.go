cd8a6bfe1cbb0944cabb3d772bf54bf281570f5f
Jinyao Ma <jinyao.ma@outlook.com>
2023-09-21 13:57:23 +0800

    .

--- backend/api/api.go
+++ backend/api/api.go
@@ -1,1 +1,19 @@
 package api
+
+// @title My App (backend/api)
+// @version 0.0.0
+// @description "My App is a continuously updated personal service collection."
+
+// @contact.name GitHub Discussions
+// @contact.url https://github.com/jinyaoMa/my-app/discussions
+
+// @license.name MIT
+// @license.url https://github.com/jinyaoMa/my-app/blob/main/LICENSE
+
+// @schemes https
+// @BasePath /api
+
+// @securityDefinitions.apikey BearerToken
+// @in header
+// @name Authorization
+// @description Authorization Header should contain value started with "Bearer " and followed by a JSON Web Token.
