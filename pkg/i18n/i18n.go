4d7d259e1480659c33a6ee0c2a65fd91aca7adef
Jinyao Ma <jinyao.ma@outlook.com>
2023-11-30 18:06:24 +0800

    .

--- pkg/i18n/i18n.go
+++ pkg/i18n/i18n.go
@@ -1,9 +1,24 @@
 package i18n
 
-import "os"
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+)
 
 type I18n[TTranslation ITranslation] struct {
-	options *I18nOptions
+	options      *I18nOptions
+	availLocales []string
+	translations []TTranslation
+}
+
+func (i18n *I18n[TTranslation]) Load() error {
+	return filepath.WalkDir(i18n.options.APath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
 func NewI18n[TTranslation ITranslation](options *I18nOptions) (i18n *I18n[TTranslation], err error) {
