41dc6bbd50e0deae0cdd9be7bf2c82f285c0d257
Jinyao Ma <jinyao.ma@outlook.com>
2023-12-09 09:40:43 +0800

    .

--- pkg/base/code.go
+++ pkg/base/code.go
@@ -3,7 +3,10 @@ package base
 import "strings"
 
 var (
-	CodeDigits = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	CodeDigits    = strings.Split("0123456789", "")
+	CodeHexDigits = strings.Split("0123456789ABCDEF", "")
+	CodeLetters   = strings.Split("abcdefghijklmnopqrstuvwxyz", "")
+	CodeULetters  = strings.Split("ABCDEFGHIJKLMNOPQRSTUVWXZ", "")
 )
 
 func GenerateCode(size uint, chars ...string) string {
